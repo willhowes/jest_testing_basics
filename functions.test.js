5e379d0032b0048ee040029dbdfde831ffc62264
eldenhowes <williameldenhowes@gmail.com>
2019-10-11 10:23:31 +0100

    Added some more example tests

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,28 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
-test('Adds 2 + 2 to equal 4', () => {
-  expect(functions.add(2, 2)).toBe(4)
-})
+// toBe - is for primative types (like integers. will not work on objects. Use toEqual instead for objects)
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// not
+test("Adds 2 + 2 to not equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// toBeNull
+test("Should be null", () => {
+  expect(functions.isNull()).toBe(null);
+});
+
+// toBeFalsy
+test("Should be falsey", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test("User should be Will howes object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Will",
+    lastName: "Howes"
+  });
+});
